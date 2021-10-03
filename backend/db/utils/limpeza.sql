6035824d047d8d58f1619d4c7e4686b6aa9483b2
Miler Grudtner Boell <34896180+Miler1@users.noreply.github.com>
2021-10-02 23:14:45 -0300

    database scripts

---
+++ backend/db/utils/limpeza.sql
@@ -0,0 +1,7 @@
+TRUNCATE licenciamento.atividade CASCADE;
+TRUNCATE licenciamento.caracterizacao CASCADE;
+TRUNCATE licenciamento.contato CASCADE;
+TRUNCATE licenciamento.documento CASCADE;
+TRUNCATE licenciamento.endereco CASCADE;
+TRUNCATE licenciamento.grupo_caracterizacao CASCADE;
+TRUNCATE analise.documento CASCADE;
